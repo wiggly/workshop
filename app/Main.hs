79ab1bd5a9916f730474b53da7bc32a3488e3c77
Nigel Rantor <wiggly@wiggly.org>
2016-05-18 01:02:49 +0100

    Added annotations for sections covered in first session

--- app/Main.hs
+++ app/Main.hs
@@ -7,21 +7,57 @@ ackerman _ 0 2 = 1
 ackerman m 0 _ = m
 ackerman m n p = ackerman m (ackerman m (n-1) p) (p-1)
 
+-- 'illuminati' is a function returning an Int
+--
+-- since it has a return value and no parameters and it is a pure function
+-- it is effectively a constant
 illuminati :: Int
 illuminati = 23
 
+-- 'answer' is a function returning an Int
+--
+-- as above, it is effectively a constant
 answer :: Int
 answer = 42
 
+-- 'squared' is a function taking an Int and returning an Int
+--
+-- it names one parameter on the left hand side 'n' and uses it in the formula
+-- on the right hand side
 squared :: Int -> Int
-squared n = undefined
+squared n = n * n
 
+-- 'plusFive' is a function taking an Int and returning an Int
+--
+-- no argument is named on the left-hand side. We have taken a function
+-- '+' with signature (Int -> Int -> Int) and applied one argument to it
+-- yielding a function with the signature (Int -> Int) which is exactly
+-- what we need for 'plusFive'
+--
+-- This is partial application and indicates that our functions are actually
+-- all the same in that they take one argument and return one value...functions
+-- with multiple arguments are really just sugar...
 plusFive :: Int -> Int
-plusFive n = undefined
+plusFive = (5+)
 
+-- squaredPlusFive is a function that accepts an Int and returns an Int
+-- no parameter is named on the left hand side
+--
+-- this is 'point-free' style, meaning we do not name values, instead we construct
+-- a value (in this case another function) by composition
+--
+-- The composition of plusFive and squared is a function with the correct signature
+-- (Int -> Int) so we can simply say that squaredPlusFive is equal to the composition
+-- of those two functions
 squaredPlusFive :: Int -> Int
-squaredPlusFive n = undefined
+squaredPlusFive = plusFive . squared
 
+-- ignore the return type of this function for now.
+--
+-- we are using some simple IO routines to print out results.
+--
+-- parenthesis are used to change the order of evaluation
+-- (as with mathematical formulae)
 composeMain :: IO ()
 composeMain = do
   print "2 squared"
@@ -31,24 +67,54 @@ composeMain = do
   print "2 squared plus five"
   print (squaredPlusFive 2)
 
+-- 'underTen' is a function returning a list of Ints
+--
+-- The list is created using the list CONStruction operator ':'
+-- that takes an element and prepends it to the head of a list.
+--
+-- the empty square brackets are the empty list.
+-- this function returns the empty list, with 9 prepended,
+-- with 8 prepended, with 7 prepended...et cetera
 underTen :: [Int]
 underTen = 1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : []
 
+-- 'underTen'' is a function returning a list of Ints
+--
+-- The list is created using some syntactic sugar but is exactly
+-- the same as the above definition.
 underTen' :: [Int]
 underTen' = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+-- 'naturals' is a function returning an infinite list of Ints
 naturals :: [Int]
 naturals = [1..]
 
+-- 'squares' is a function returning a list of Ints
+--
+-- this list is created by squaring each element of the list returned
+-- by 'naturals' and is therefore also an infinite list
 squares :: [Int]
-squares = undefined
+squares = map squared naturals
 
+-- 'intSum' is a function taking a list of Ints and returning an Int
+--
+-- it uses a 'fold' that takes a function, a starting value and a
+-- list and collapses/folds the list elements using the function,
+-- squashing it down to one value
+--
+-- The operation here is addition since we want a sum and the starting
+-- value is the identity for addition, 0 (zero)
 intSum :: [Int] -> Int
-intSum = undefined
+intSum = foldr (+) 0
 
+-- 'intProduct' is a function taking a list of Ints and returning an Int
+--
+-- as above but since we want the product we use multiplication and the identity
+-- for multiplication, 1
 intProduct :: [Int] -> Int
-intProduct = undefined
+intProduct = foldr (*) 1
 
+-- TODO - WRITE THIS YOURSELF!?!
 myMap :: (a -> b) -> [a] -> [b]
 myMap = undefined
 
